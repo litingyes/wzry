6b8f34f3511e4522a0ef78e9b2781ae088be6486
litingyes <luz.liting@gmail.com>
2025-05-13 00:07:04 +0800

    chore(analytics): add vercel analytics

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
+<script lang="ts" setup>
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <Analytics />
   </div>
 </template>
