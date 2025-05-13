5d9bfb74f159b2e0d001ae530258a7723300e913
litingyes <luz.liting@gmail.com>
2025-05-13 08:33:10 +0800

    feat(hero link): add utils to get hero images

---
+++ utils/heroLink.ts
@@ -0,0 +1,5 @@
+export const getHeroLink = (heroId:number) => 
+  `https://game.gtimg.cn/images/yxzj/img201606/heroimg/${heroId}/${heroId}.jpg`
+
+export const getHeroBigSkin = (heroId:number,skinNumber:number) =>
+  `https://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/${heroId}/${heroId}-bigskin-${skinNumber}.jpg`
