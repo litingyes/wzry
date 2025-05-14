10d60eea43b6859ab28e0df14092a73b8183b159
litingyes <luz.liting@gmail.com>
2025-05-14 22:50:42 +0800

    style(scrollbar): adjust style

--- app.vue
+++ app.vue
@@ -18,27 +18,14 @@ import '@unocss/reset/tailwind-compat.css'
 
 <style lang="scss">
 ::-webkit-scrollbar {
-  @apply size-10;
-  width: 40px;
+  --at-apply: size-1;
 }
 
 ::-webkit-scrollbar-track {
-  background: #f1f1f1;
-  border-radius: 4px;
+  --at-apply: bg-gray-200 rounded-lg;
 }
 
 ::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 4px;
-
-  &:hover {
-    background: #555;
-  }
-}
-
-/* Firefox 滚动条样式 */
-* {
-  scrollbar-width: thin;
-  scrollbar-color: #888 #f1f1f1;
+  --at-apply: 'bg-gray-300 rounded-lg transition-colors hover:bg-gray-400';
 }
 </style>
--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,7 @@
+import { mergeConfigs } from '@unocss/core'
+import { transformerDirectives } from 'unocss'
 import config from './.nuxt/uno.config.mjs'
 
-export default config
+export default mergeConfigs([config, {
+  transformers: [transformerDirectives()],
+}])
